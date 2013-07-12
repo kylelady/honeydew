44cc64cfc26f538885f4b641fa3c3da6ba5d38ef
Kyle Lady <kylelady@umich.edu>
2013-07-11 22:28:38 -0400

    reshuffling files around to enable relative importing. might not actually work right now.

---
+++ honeydew/backends/filesystem.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from ..database import Backend
+
+class Filesystem(Backend):
+    '''Simple filesystem-based backend implementation'''
+    pass
---
+++ honeydew/main.py
@@ -0,0 +1,4 @@
+#/usr/bin/env python
+
+import backends.filesystem
+import database
