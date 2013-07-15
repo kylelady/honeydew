234254456a37dfcd2868cf7e3eaf7ffa84012bf2
Kyle Lady <kylelady@umich.edu>
2013-07-15 10:15:06 -0400

    more work on fs impl and db interface

--- honeydew/backends/filesystem.py
+++ honeydew/backends/filesystem.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 
-from ..database import Backend
+from honeydew.database import Backend
 
 class Filesystem(Backend):
     '''Simple filesystem-based backend implementation'''
     pass
+
+    class FSStudent(Backend.Student):
+        def __init__(self, config, uniqname):
+            self._config = config
+            self._uniqname = uniqname
+            self._open(uniqname)
+
+        def __enter__(self):
+            pass
+
+        def __exit__(self):
+            self.close()
+
+        def _open(self, uniqname):
+
+
+        def close(self):
+
--- honeydew/database.py
+++ honeydew/database.py
@@ -11,7 +11,7 @@ class Database(object):
         self._db = backend
 
     def grades(self, uniqname):
-        return self._db._student(self._config, uniqname)
+        return self._db.get_student(self._config, uniqname)
 
 
 class Backend(object):
@@ -19,18 +19,32 @@ class Backend(object):
     Interface for various backends to store/retrive student projects/records
     '''
 
+    def __init__(self, config):
+        '''init'''
+        raise NotImplementedError()
+
     class Student(object):
         '''
         context-managed class for student data
         '''
 
         def __init__(self, config, uniqname, **kwargs):
-            pass
-
-        def __enter__(self):
             '''prepare to receive student data'''
             raise NotImplementedError()
 
-        def __exit__(self):
-            '''write data out to disk or postgres or something'''
+        def __enter__(self):
             raise NotImplementedError()
+
+        def __exit__(self):
+            '''call any cleanup needed, including close()'''
+            raise NotImplementedError()
+
+        def close(self):
+            '''
+            write data out to disk or postgres or something
+
+            don't implement this if you want to force use within a context
+            manager
+            '''
+            raise NotImplementedError()
+
