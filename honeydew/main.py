e97ca0399eec9199ffaaf0ccf5a03ace58030cbc
Kyle Lady <kylelady@umich.edu>
2013-07-16 10:58:10 -0400

    framework of main()s that seems to make sense. I think.
    outer main does argparsing and validation, and then passes args to main w/i pkg

---
+++ honeydew/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+
+import honeydew.backends
+import honeydew.database
+
+
+def main(args):
+    '''
+    Requires a dict of runtime configuration
+
+    Minimum configuration:
+    --pass--
+    '''
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 #/usr/bin/env python
 
+import argparse
+import os
+import sys
+
 import honeydew.backends.filesystem
 import honeydew.database
+import honeydew.main
+
+
+def main(argv):
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
