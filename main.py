2daf2196c47a96b26ccc5fcd18c753bf0b2f3855
Kyle Lady <kylelady@umich.edu>
2013-07-21 15:57:03 -0400

    note to self: run code before pushing to github

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--backend', choices=backends, default='filesystem',
             help='select backend engine; defaults to filesystem')
-    parser.add_argument(['-c', '--config'], default='eecs280.cfg', type=open,
+    parser.add_argument('-c', '--config', default='eecs280.cfg', type=open,
             help='config file; see sample.cfg')
-    parser.add_argument('project_num', dest='n',
+    parser.add_argument('n', metavar='project_num',
             help='which project the command should be run on')
+    parser.parse_args(argv[1:])
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
